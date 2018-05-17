5d3a20f8e09334ebd1dbc63e3c20a564316dad42
yejunyou <ye_junyou@163.com>
2018-05-17 21:14:26 +0800

    add divide image functioin

---
+++ YYDivideImage/Extension/UIImage+Divide.swift
@@ -0,0 +1,66 @@
+//
+//  UIImage+Divide.swift
+//  YYDivideImage
+//
+//  Created by yejunyou on 2018/5/16.
+//  Copyright © 2018年 futureversion. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    public func divide(row: Int, col: Int) -> [UIImage]? {
+
+        let originSize = self.size;
+        let divideW: CGFloat = originSize.width/CGFloat(col);
+        let divideH: CGFloat = originSize.width/CGFloat(col);
+
+        var imageArray: [UIImage] = []
+        
+        for i in 0..<col*row {
+            let indexX: CGFloat = CGFloat(i%col)
+            let indexY: CGFloat = CGFloat(i/row)
+            let divideRect = CGRect.init(x: indexX*divideW, y: indexY*divideH, width: divideW, height: divideH)
+            if let divideImage = self.divideImage(rect: divideRect) {
+                imageArray.append(divideImage)
+            }
+        }
+        
+        return imageArray
+    }
+    
+    // MARK: 私有方法
+    /// 裁剪
+    private func divideImage(rect: CGRect) -> UIImage? {
+        if let imageRef: CGImage = self.cgImage {
+            if let newCGImage = imageRef.cropping(to: rect) {
+                return UIImage(cgImage: newCGImage)
+            } else {
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+    
+    func zip() -> UIImage {
+        let data = UIImageJPEGRepresentation(self, 0.5)
+        return UIImage(data: data!)!
+    }
+    
+    func getDataSize() -> Int {
+        let data = UIImageJPEGRepresentation(self, 1.0)
+        //        let data = UIImagePNGRepresentation(self)
+        print("\((data?.count)! / 1024)KB")
+        return (data?.count)! / 1024
+    }
+    
+    func resize(maxSize: Int) -> UIImage? {
+        let size = self.getDataSize()
+        let time = CGFloat(size) / CGFloat(maxSize)
+        let zip = CGFloat(1) / time
+        let data = UIImageJPEGRepresentation(self, zip)
+        return UIImage(data: data!)
+    }
+}
--- YYDivideImage/YYDivideImageViewController.swift
+++ YYDivideImage/YYDivideImageViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class YYDivideImageViewController: UIViewController {
     
     private var image: UIImage
+    private var divideImages: [UIImage]?
+    private var imageCount: Int?
     
+    // MARK: init ==method==
     init(image: UIImage) {
         self.image = image
         super.init(nibName: nil, bundle: nil)
-        self.imageView.image = image
+//        self.imageView.image = image
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -24,29 +27,57 @@ class YYDivideImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "YY分图"
+        view.backgroundColor = UIColor.gray
         addSubviews()
         layout()
+        
+        // 默认3x3
+        refreshLayout(row: 3, col: 3)
     }
     
     private func addSubviews(){
-        view.addSubview(imageView)
+//        view.addSubview(imageView)
     }
     
     private func layout(){
-        imageView.snp.makeConstraints { (make) in
-            make.left.equalTo(15)
-            make.right.equalTo(-15)
-            make.top.equalTo(44)
-            make.bottom.equalTo(-15)
+//        imageView.snp.makeConstraints { (make) in
+//            make.left.equalTo(15)
+//            make.right.equalTo(-15)
+//            make.top.equalTo(44)
+//            make.bottom.equalTo(-15)
+//        }
+    }
+    
+    // MARK: ==private method==
+    private func refreshLayout(row: Int, col: Int) {
+        divideImages?.removeAll();
+        divideImages = self.image.divide(row: row, col: col);
+        var index: Int = 0
+        
+        let margin: CGFloat = 1;
+        let imageW: CGFloat = (view.bounds.size.width - margin *  CGFloat(col + 1)) / CGFloat(col);
+        let imageH: CGFloat = imageW;//(view.bounds.size.height - margin *  CGFloat(row + 1)) / CGFloat(row);
+//        CGFloat imageW = CGFloat(self.image.getDataSize());
+        var imageX: CGFloat = 0;
+        var imageY: CGFloat = 0;
+        
+        for i in 0..<row*col {
+            let indexX: CGFloat = CGFloat(i%col)
+            let indexY: CGFloat = CGFloat(i/row)
+            imageX = margin + imageW * indexX + indexX * margin
+            imageY = 64 + imageH * indexY + indexY * margin
+            let iv: UIImageView = UIImageView.init(frame: CGRect.init(x: imageX, y: imageY, width: imageW, height: imageH))
+            view.addSubview(iv)
+            iv.image = divideImages?[index]
+            index += 1
+            
         }
     }
     
-    // MARK : LAZY ADD
-    lazy private var imageView: UIImageView = {
-        let iv = UIImageView.init()
-        iv.backgroundColor = UIColor.yellow
-        return iv
-    }()
+    // MARK: ==LAZY ADD==
+//    lazy private var imageView: UIImageView = {
+//        let iv = UIImageView.init()
+//        iv.backgroundColor = UIColor.yellow
+//        return iv
+//    }()
 }
